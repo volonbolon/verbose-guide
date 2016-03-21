12e872d05043098b0b9d4e31de8f712aeaa23786
Ariel Rodriguez <ariel@volonbolon.me>
2016-03-21 17:41:34 -0300

    Add Xcode Project

---
+++ VerboseGuide/VerboseGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VerboseGuide
+//
+//  Created by Ariel Rodriguez on 3/21/16.
+//  Copyright © 2016 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
