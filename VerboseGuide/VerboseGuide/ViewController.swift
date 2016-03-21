22cfd164e0e131dfeb0a33744b0615fa6eedda9c
Ariel Rodriguez <ariel@volonbolon.me>
2016-03-21 18:09:51 -0300

    Present safari view controller with login form

--- VerboseGuide/VerboseGuide/ViewController.swift
+++ VerboseGuide/VerboseGuide/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let url = NSURL(string: "https://rawgit.com/volonbolon/3bd9e3c43e60c30992ee/raw/57de598f3197ba639607d312c8e0e8b810af63e0/login.html")
+        
+        let svc = SFSafariViewController(URL: url!)
+        self.presentViewController(svc, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
